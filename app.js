9e30f5075b6621cf63732f53d66a2e845f8c4f1f
Cecily Grossmann <grssmnn2@gmail.com>
2018-01-07 16:11:20 -0600

    initial set up

---
+++ app.js
@@ -0,0 +1,12 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCu-hVPCfk5n5ZAZ1JczJ-CLuw-AOLrYoU",
+    authDomain: "trainapp-e2286.firebaseapp.com",
+    databaseURL: "https://trainapp-e2286.firebaseio.com",
+    projectId: "trainapp-e2286",
+    storageBucket: "trainapp-e2286.appspot.com",
+    messagingSenderId: "1093165244921"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();

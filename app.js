94691b4c12d5c2b9bef5e603ece63eed46ebd940
Cecily Grossmann <grssmnn2@gmail.com>
2018-01-07 17:22:23 -0600

    adding user input to table

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
- // Initialize Firebase
- var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyCu-hVPCfk5n5ZAZ1JczJ-CLuw-AOLrYoU",
     authDomain: "trainapp-e2286.firebaseapp.com",
     databaseURL: "https://trainapp-e2286.firebaseio.com",
     projectId: "trainapp-e2286",
     storageBucket: "trainapp-e2286.appspot.com",
     messagingSenderId: "1093165244921"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
+
+$("#submit").on("click", function () {
+    event.preventDefault();
+   
+    var tableData = $("<td>");
+    
+    tableData.append($("#trainName").val());
+    tableData.append($("#destination").val());
+   tableData.append($("#firstTrain").val());
+   tableData.append($("#frequency").val());
+    
+    $("#trainSchedule").append(tableData);
+
+})

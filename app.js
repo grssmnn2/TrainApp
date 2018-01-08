0d95467da56f22bb238baab8145d07220ce1b18f
Cecily Grossmann <grssmnn2@gmail.com>
2018-01-08 14:14:22 -0600

    empty text boxes on submit

--- app.js
+++ app.js
@@ -10,17 +10,58 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+// var arrive;
+// var minutesAway;
 
+// document.onload(function(){
+//     arrive = moment().endOf('day').fromNow();
+//     minutesAway = moment().add(frequency, 'minutes').calendar();
+// })
+
+// $("#chooArrive").append(arrive);
+// $("#chooAway").append(minutesAway);
+
+
+
+//  when the submit button is clicked
 $("#submit").on("click", function () {
+    // prevent submit button from opening new page
     event.preventDefault();
-   
-    var tableData = $("<td>");
-    
-    tableData.append($("#trainName").val());
-    tableData.append($("#destination").val());
-   tableData.append($("#firstTrain").val());
-   tableData.append($("#frequency").val());
-    
-    $("#trainSchedule").append(tableData);
+    // send data to firebase
+    database.ref().set({
+        name: $("#trainName").val(),
+        destination: $("#destination").val(),
+        firstTrain: $("#firstTrain").val(),
+        frequency: $("#frequency").val(),
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    })
 
+    // add new table data to bottom of table
+    var tableRow = $("<tr>");
+    
+    // append the train name input, destination, first train, frequency
+    tableRow.append("<td>" + $("#trainName").val() + "</td>");
+    tableRow.append("<td>" + $("#destination").val() + "</td>");
+    tableRow.append("<td>" + $("#firstTrain").val() + "</td>");
+    tableRow.append("<td>" + $("#frequency").val() + "</td>");
+    // append table data to the train schedule
+    $("#trainSchedule").append(tableRow);
+
+    // empty input after submit is clicked
+    $("#trainName").val("");
+    $("#destination").val("");
+    $("#firstTrain").val("");
+    $("#frequency").val("");
+    
 })
+
+// pull data updates from Firebase and append them to the table
+database.ref().on("value", function(snapshot){
+    $(tableRow).append(snapshot.val().name);
+    $(tableRow).append(snapshot.val().destination);
+    $(tableRow).append(snapshot.val().firstTrain);
+    $(tableRow).append(snapshot.val().frequency);
+       
+});
+
+$("#trainSchedule").append(tableData);
